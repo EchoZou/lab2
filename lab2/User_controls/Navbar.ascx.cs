7696af8095d559261fe03c1db719eb4e90e08796
EchoZou <200271124@student.georgianc.on.ca>
2016-05-30 14:47:12 -0400

    lab2

---
+++ lab2/User_controls/Navbar.ascx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/*
+ * name: Mo Zou
+ * date: May 30, 2016
+ * add the setactivepage method
+ * 
+ */ 
+
+
+namespace lab2
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+
+        /*
+         * This method adds a css class of "active" to list items related 
+         * to navigation links of each page
+         * 
+         * @method SetActivePage
+         * @return (void)
+         * 
+         */ 
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
